6b27f53b43bcff2e8c98e87293f2fa0a033b7f6d
jaaknoxucsc <165941474+jaaknoxucsc@users.noreply.github.com>
2024-04-17 14:55:11 -0700

    Update asgn5a.js

--- assignment5/main/asgn5a.js
+++ assignment5/main/asgn5a.js
@@ -1,3 +1,6 @@
+import * as THREE from 'https://unpkg.com/three@0.128.0/build/three.module.js';
+import { OBJLoader } from 'https://threejs.org/examples/jsm/loaders/OBJLoader.js';
+
 // Scene setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -7,7 +10,7 @@ document.body.appendChild(renderer.domElement);
 
 // Texture and OBJ Loaders
 const loader = new THREE.TextureLoader();
-const objLoader = new THREE.OBJLoader();
+const objLoader = new OBJLoader();
 
 // Load and display a static .obj model with texture
 objLoader.load('12140_Skull_v3_L2.obj', function(object) {

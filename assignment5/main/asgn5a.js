120df7bf7ef8a0dd0c504fa0300f983bf065a6e1
jaaknoxucsc <165941474+jaaknoxucsc@users.noreply.github.com>
2024-04-17 17:14:03 -0700

    Update asgn5a.js

--- assignment5/main/asgn5a.js
+++ assignment5/main/asgn5a.js
@@ -1,2 +1,2 @@
-import * as THREE from 'https://unpkg.com/three@0.128.0/build/three.module.js';
-import { OBJLoader } from 'https://threejs.org/examples/jsm/loaders/OBJLoader.js';
+import * as THREE from 'three';
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
